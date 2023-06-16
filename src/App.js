d362da60ee20b51817d20be0aa7bfbd87667a1c6
Kleber Molina Kardel <molinakardel@gmail.com>
2023-06-16 13:33:03 -0300

    Cria header com menu de navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const menuItems = [
+    {
+      id: 1,
+      title: "Home"
+    },
+    {
+      id: 2,
+      title: "Sobre"
+    },
+    {
+      id: 3,
+      title: "Portfolio"
+    },
+    {
+      id: 4,
+      title: "Contato"
+    }
+  ]
+
   return (
     <div className="App">
-      
+      <header>
+        <nav>
+          <h2>Meu Projeto</h2>
+        </nav>
+
+        <ul>
+          {menuItems.map(menu => (
+            <li><a href={`#${menu.title}`}>{menu.title}</a></li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
